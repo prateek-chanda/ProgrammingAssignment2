a7ba5276aebb21ffbd0048c80c9629fb3f4bf562
prateek-chanda <69039935+prateek-chanda@users.noreply.github.com>
2020-11-29 01:20:13 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix is a function that creates an R object that stores a matrix and its inverse.
+## It can be used to instantiate an object that returns a set of 4 functions in a list and
+## two data objects, x and i
 makeCacheMatrix <- function(x = matrix()) {
 
         m <- NULL
@@ -18,7 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-
+## cacheSolve requires an argument from makeCacheMatrix to retrieve the inverse of the matrix
+## from the cache stored in makeCacheMatrix environment. If the inverse was never calculated and
+## stored in the cache, this function will calculate the inverse and store it within makeCacheMatrix
+## environment
 cacheSolve <- function(x, ...) {
             
         m <- x$getinverse()
